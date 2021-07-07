c7e8a727a54ea1bb8c9e0b143506042a1fd46338
Alex7498-SV <00068817@uca.edu.sv>
2021-07-07 17:25:42 -0600

    Ahi te va chino
    Mapa controller y vista

---
+++ src/main/java/com/capas/uca/parcial3/controller/MapaController.java
@@ -0,0 +1,35 @@
+package com.capas.uca.parcial3.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.HttpSessionRequiredException;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MapaController {
+	
+	@Autowired
+	private MainController maincontroller;
+	
+	@RequestMapping("/mapa")
+	public ModelAndView mapa(HttpSession request) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mapa");
+		return mav;
+	}
+}
